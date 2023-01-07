71b1bcb0658554021ecc0bca66e525e0efccdd63
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-01-07 15:31:54 +0530

    The Celebrity Problem

---
+++ 304.cpp
@@ -0,0 +1,75 @@
+//The Celebrity Problem (see copy and vid https://www.youtube.com/watch?v=CiiXBvrX-5A)
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+       stack<int>s;
+       for(int i=0;i<n;i++)
+       {
+           s.push(i);
+       }
+       while(s.size()!=1)
+       {
+           int a=s.top();
+           s.pop();
+           int b=s.top();
+           s.pop();
+           if(M[a][b]==1)
+           {
+               s.push(b);
+           }
+           else if(M[b][a]==1)
+           {
+               s.push(a);
+           }
+           
+       }
+       for(int i=0;i<n;i++)
+       {   
+         if(s.top()!=i) //omit diagonal element
+         {
+             if(M[s.top()][i]==1 || M[i][s.top()]==0) //ulti condn likhi hai coz agar ek bhi not satisfied we get -1
+                return -1;
+         }
+                
+       }
+       return s.top();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+
+// } Driver Code Ends
