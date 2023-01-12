58c839aae11ab726bb824c1ff5fae5b61e2e8ca8
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-01-12 13:31:02 +0530

    reverse a queue

---
+++ 321.cpp
@@ -0,0 +1,54 @@
+//Reverse a Queue (see copy)
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+queue<int> rev(queue<int> q);
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    queue<int> a=rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
+
+
+//function Template for C++
+
+//Function to reverse the queue.
+
+void reverse(queue<int>& q)
+{
+    if(q.empty())
+        return;
+    
+    int fr=q.front();
+    q.pop();
+    
+    reverse(q);
+    q.push(fr);
+    
+}
+queue<int> rev(queue<int> q)
+{
+    reverse(q);
+    return q;
+}
