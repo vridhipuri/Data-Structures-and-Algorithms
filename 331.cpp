ad2c8dfb678949cf790b43807fb3ff9fa768841c
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-01-14 14:25:11 +0530

    First non-repeating character in a stream

---
+++ 331.cpp
@@ -0,0 +1,54 @@
+//First non-repeating character in a stream(see copy)
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    
+		    unordered_map<char,int>m;
+		    queue<char>q;
+		    string ans="";
+		    for(int i=0;i<A.length();i++)
+		    {
+		        char ch=A[i];
+		        m[ch]++;
+		        
+		        q.push(ch);
+		        
+		        while(!q.empty())
+		        {
+		            if(m[q.front()]>1)
+		            {
+		                q.pop();
+		            }
+		            else{
+		                ans.push_back(q.front());
+		                break;
+		            }
+		        }
+		        //when queue becomes empty
+		        if(q.empty())
+		            ans.push_back('#');
+		    }
+		    return ans;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
