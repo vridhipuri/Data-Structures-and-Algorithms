3e980a8ac41488e8d7846f0fb83daea394f4d1f0
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-01-15 21:32:23 +0530

    Kadane's Algorithm

---
+++ 13.cpp
@@ -0,0 +1,58 @@
+//find Largest sum contiguous Subarray [V. IMP]
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+       long sum=0;
+       long maxi=arr[0];
+       
+       for(int i=0;i<n;i++)
+       {
+           sum=sum+arr[i];
+           maxi=std::max(sum,maxi);
+           
+           if(sum<0)
+           {
+               sum=0;
+           }
+       }
+       return maxi;
+    
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
