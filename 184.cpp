732fc5c816210c58601e8a49c9c76b03fdc11b94
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-01-17 18:23:18 +0530

    Count Leaf Nodes

---
+++ 184.cpp
@@ -0,0 +1,36 @@
+//Count Leaf Nodes https://www.codingninjas.com/codestudio/problems/count-leaf-nodes_893055?source=youtube&campaign=LoveBabbar_Codestudio&utm_source=youtube&utm_medium=affiliate&utm_campaign=LoveBabbar_Codestudio&leftPanelTab=0
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+    
+***********************************************************/
+void inorder(BinaryTreeNode<int> *root, int& count)
+{
+    if(root==NULL)
+        return ;
+    inorder(root->left,count);
+    if(root->left==NULL && root->right==NULL)
+        count++;
+    inorder(root->right,count);
+
+}
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    
+    int count=0;
+    inorder(root,count);
+    return count;
+}
