84e33b81d37ab80f9811d6e898f569708821cc8c
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-03-11 14:36:09 +0530

    DFS of Graph

---
+++ 359.cpp
@@ -0,0 +1,70 @@
+//Implement DFS Algo (see copy)
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    
+    void dfs(int node,unordered_map<int,bool> &visited,vector<int> &ans, vector<int> adj[])
+    {
+        visited[node]=true; //mark first/starting node as true
+        
+        ans.push_back(node);
+        //make recursive call for every connected node
+        for(auto i:adj[node])
+        {
+            if(!visited[i])
+            {
+                dfs(i,visited,ans,adj);
+            }
+        }
+    }
+    
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>ans;
+        unordered_map<int,bool>visited;
+        //for loop as agar graph disconnected hua toh <V krke saare nodes traverse ho jaayenge
+        for(int i=0;i<V;i++)
+        {
+            if(visited[i]==false) //checking each node 
+            {
+                dfs(i,visited,ans,adj);
+            }
+        }
+        return ans;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
