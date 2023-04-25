51aa9f1bcd50b1b1a99ce267bd3aef9f045d0ffc
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-04-25 23:46:58 +0530

    Middle of Three

---
+++ 104.cpp
@@ -0,0 +1,36 @@
+//Maximum and minimum of an array using minimum number of comparisons
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        
+       if (A < B)
+        return (B < C)? B : max(A, C);
+      return (A < C)? A : max(B, C);
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
