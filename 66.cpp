7fa04190608f985ad60c13007be1692e5a4aa2e5
vridhipuri <93861241+vridhipuri@users.noreply.github.com>
2023-06-07 16:00:59 +0530

    Split the Binary string into two substring with equal 0’s and 1’s

---
+++ 66.cpp
@@ -0,0 +1,60 @@
+//Split the Binary string into two substring with equal 0’s and 1’s
+
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int maxSubStr(string str){
+        //Write your code here
+        
+        int n=str.length();
+        int count0 = 0, count1 = 0;
+
+    // To store the count of maximum
+    // substrings str can be divided into
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (str[i] == '0') {
+            count0++;
+        }
+        else {
+            count1++;
+        }
+        if (count0 == count1) {
+            cnt++;
+        }
+    }
+
+    // It is not possible to
+    // split the string
+    if (count0!=count1) {
+        return -1;
+    }
+
+    return cnt;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin >> str;
+	    Solution ob;
+	    int ans = ob.maxSubStr(str);
+	    cout << ans<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
